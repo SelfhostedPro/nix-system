1ee048640e4e40cc8d59dddf682d1497429fbcaf
SelfhostedPro <info@selfhosted.pro>
2024-04-23 13:37:59 -0700

    saving

--- hosts/default.nix
+++ hosts/default.nix
@@ -11,4 +11,12 @@
       ./desktop # Base Desktop Configuration
     ];
   };
+  live = inputs.nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = {inherit inputs outputs vars;};
+      modules = [
+        (inputs.nixpkgs + "/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix")
+        ./desktop
+      ];
+    };
 }
--- modules/security/tools.nix
+++ modules/security/tools.nix
@@ -8,8 +8,17 @@
   ];
 
   environment.systemPackages = with pkgs; [
+    # Security Tools
     xorg.xhost
     nmap
     minder
+    # IOS Tools
+    ideviceinstaller
+    libimobiledevice
+    libimobiledevice-glue
+    ifuse
+    usbmuxd
+    usbmuxd2
+    libusbmuxd
   ];
 }
